bc3e054c469bfb4d6d517db342aa4afe23aee72b
i211554 <i211554@nu.edu.pk>
2024-12-05 11:45:22 +0500

    commit by i211554

---
+++ calc.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    char operation;
+    double num1, num2, result;
+
+    cout << "Simple Calculator\n";
+    cout << "-----------------\n";
+    cout << "Enter an operator (+, -, *, /): ";
+    cin >> operation;
+
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    switch (operation) {
+        case '+':
+            result = num1 + num2;
+            cout << "Result: " << num1 << " + " << num2 << " = " << result << endl;
+            break;
+        case '-':
+            result = num1 - num2;
+            cout << "Result: " << num1 << " - " << num2 << " = " << result << endl;
+            break;
+        case '*':
+            result = num1 * num2;
+            cout << "Result: " << num1 << " * " << num2 << " = " << result << endl;
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                cout << "Result: " << num1 << " / " << num2 << " = " << result << endl;
+            } else {
+                cout << "Error: Division by zero is not allowed.\n";
+            }
+            break;
+        default:
+            cout << "Error: Invalid operator. Please use +, -, *, or /.\n";
+    }
+
+    return 0;
+}
